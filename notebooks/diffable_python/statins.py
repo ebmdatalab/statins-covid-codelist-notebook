6607dc99a54eec564bff3263414dc8c6c31be9c6
Brian <brianmkcebm@gmail.com>
2020-04-11 15:52:08 +0100

    update and finalise
    add in combination products

--- notebooks/diffable_python/statins.py
+++ notebooks/diffable_python/statins.py
@@ -16,7 +16,7 @@
 
 # This notebook contains SnoMed/NHS [dm+d codes](https://ebmdatalab.net/what-is-the-dmd-the-nhs-dictionary-of-medicines-and-devices/) for statins derived from the BNF code section on [lipid regulating drugs](https://openprescribing.net/bnf/0212/). 
 #
-# Combination products have not been included. TBC with team.
+#
 
 from ebmdatalab import bq
 import os
@@ -26,10 +26,12 @@ import pandas as pd
 sql = '''WITH bnf_codes AS (
   SELECT bnf_code FROM hscic.presentation WHERE 
 (bnf_code LIKE '0212000B0%' OR #  Atorvastatin
-bnf_code LIKE '0212000M0%' OR #  Fluvastatin Sodium
-bnf_code LIKE '0212000X0%' OR #  Pravastatin Sodium
-bnf_code LIKE '0212000AA%' OR #  Rosuvastatin Calcium
-bnf_code LIKE '0212000Y0%')    #  Simvastatin
+bnf_code LIKE  '0212000AJ%' OR #  Fenofibrate/Simvastatin 
+bnf_code LIKE  '0212000M0%' OR #  Fluvastatin Sodium
+bnf_code LIKE  '0212000X0%' OR #  Pravastatin Sodium
+bnf_code LIKE  '0212000AA%' OR #  Rosuvastatin Calcium
+bnf_code LIKE  '0212000Y0%' OR #  Simvastatin
+bnf_code LIKE  '0212000AC%')   #  Simvastatin & Ezetimibe 
 )
 SELECT "vmp" AS type, id, bnf_code, nm
 FROM dmd.vmp
@@ -47,6 +49,3 @@ statins_codelist = bq.cached_read(sql, csv_path=os.path.join('..','data','statin
 pd.set_option('display.max_rows', None)
 pd.set_option('display.width', None)
 statins_codelist
-# -
-
-
